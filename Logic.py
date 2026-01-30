d5655d4aa76dd2470bcb475deef3a5f02f816f8e
ST-TH\user <dave0629@gmail.com>
2026-01-30 11:52:51 +0800

    Input and Ouput Logic

---
+++ Logic.py
@@ -0,0 +1,52 @@
+var1 = 9
+
+if var1 >= 20:
+    print("this condition is true")
+elif var1 >= 10:
+    print("this condition is true")
+elif var1 >= 5:
+    print("this condition is true")
+elif var1 >= 1:
+    print("this condition is true")
+elif var1 >= -1:
+    print("this condition is true")
+else:
+    print("conditions are false")
+print("結束條件判斷")
+
+
+
+
+'''
+if var1 >= 10:
+    print("condition is true")
+
+
+if var1 >= 10:
+    print("condition is true")
+else:
+    print("condition is false")
+
+
+if var1 >= 20:
+    print("this condition is true")
+elif var1 >= 10:
+    print("this condition is true")
+else:
+    print("conditions are false")
+
+
+for i in range(1, 11):
+    print("i = " + str(i))
+
+i = 1
+while i <= 10:
+    print("i = " + str(i))
+    i = i + 1
+
+if i > 5:
+    break
+
+if i % 2 == 0:
+    continue
+'''
